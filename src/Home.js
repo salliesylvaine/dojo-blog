100fde52f8544728c794fe999d80440885737abe
Sallie McKee <salliemckee@Morrigan.local>
2022-06-08 12:04:01 -0500

    setting up state in Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,21 @@
-const Home = () => {
-  const handleCLick = () => {
-    console.log("hello world");
-  };
+import { useState } from "react";
 
-  const handleClickAgain = (name) => {
-    console.log("hello " + name);
+const Home = () => {
+  const [name, setName] = useState("mario");
+  const [age, setAge] = useState(25);
+
+  const handleCLick = () => {
+    setName("luigi");
+    setAge(30);
   };
 
   return (
     <div className="home">
       <h2>Home</h2>
+      <p>
+        {name} is {age} years old
+      </p>
       <button onClick={handleCLick}>CLick Me</button>
-      <button
-        onClick={() => {
-          handleClickAgain("mario");
-        }}
-      >
-        Click Me Again
-      </button>
     </div>
   );
 };
