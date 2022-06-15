d5e2eeff8290e5e5c18770e183be00c882f1b7a7
Sallie McKee <salliemckee7@gmail.com>
2022-06-15 11:16:19 -0500

    setting error messages

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import Navbar from "./Navbar";
 import Home from "./Home";
 
 function App() {
-  // const title = "Welcome to the new blog";
-  // const likes = 50;
-  // const person = { name: "yoshi", age: 30 };
-
   return (
     <div className="App">
       <Navbar />
