afb74c91c260731f89fb31d7c735f8f4d59de617
Sallie McKee <salliemckee7@gmail.com>
2022-06-16 13:19:43 -0500

    built new blog form

--- src/Create.js
+++ src/Create.js
@@ -2,6 +2,18 @@ const Create = () => {
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
+      <form>
+        <label>Blog Title:</label>
+        <input type="text" required />
+        <label>Blog Body:</label>
+        <textarea required></textarea>
+        <label>Blog Author:</label>
+        <select>
+          <option value="mario">mario</option>
+          <option value="yoshi">yoshi</option>
+        </select>
+        <button>Add Blog</button>
+      </form>
     </div>
   );
 };
